f8a54638d9b4abf9df002ae9f46650b4693ab114
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-02-10 17:26:30 -0800

    Initial commit:

--- src/routes/message.js
+++ src/routes/message.js
@@ -6,17 +6,30 @@ const Message = require('../models/message')
 
 /** Route to get all messages. */
 router.get('/', (req, res) => {
-    // TODO: Get all Message objects using `.find()`
-
-    // TODO: Return the Message objects as a JSON list
+    // Get all Message objects using `.find()`
+    Message.find()
+      .then(messages => {
+        // Return the Message objects as a JSON list
+        return res.send(messages)
+      })
+      .catch(err => {
+        throw err.message
+      })
 })
 
 /** Route to get one message by id. */
 router.get('/:messageId', (req, res) => {
-    // TODO: Get the Message object with id matching `req.params.id`
+    // Get the Message object with id matching `req.params.id`
     // using `findOne`
+    Message.findOne({ _id: req.params.messageId })
+      .then(message => {
+        // Return the matching Message object as JSON
+        return res.send(message)
+      })
+      .catch(err => {
+        throw err.message
+      })
 
-    // TODO: Return the matching Message object as JSON
 })
 
 /** Route to add a new message. */
@@ -40,17 +53,38 @@ router.post('/', (req, res) => {
 
 /** Route to update an existing message. */
 router.put('/:messageId', (req, res) => {
-    // TODO: Update the matching message using `findByIdAndUpdate`
-
-    // TODO: Return the updated Message object as JSON
+    // Update the matching message using `findByIdAndUpdate`
+    Message.findByIdAndUpdate(req.params.messageId, req.body)
+      .then(updatedMessage => {
+        // Return the updated Message object as JSON
+        return res.send(updatedMessage)
+      })
+      .catch(err => {
+        throw err.message
+      })
 })
 
 /** Route to delete a message. */
 router.delete('/:messageId', (req, res) => {
-    // TODO: Delete the specified Message using `findByIdAndDelete`. Make sure
+    // Delete the specified Message using `findByIdAndDelete`. Make sure
     // to also delete the message from the User object's `messages` array
+    Message.findByIdAndDelete(req.params.messageId)
+      .then(deletedMessage => {
+        return User.findById(deletedMessage.author)
+      })
+      .then(user => {
+        return user.messages.filter(message => {
+          return message._id.toString() != req.params.messageId.toString()
+        })
+      })
+      .then(result => {
+        // Return a JSON object indicating that the Message has been deleted
+        return res.send({ msg: "The message has been successfully deleted." })
+      })
+      .catch(err => {
+        throw err.message
+      })
 
-    // TODO: Return a JSON object indicating that the Message has been deleted
 })
 
 module.exports = router
